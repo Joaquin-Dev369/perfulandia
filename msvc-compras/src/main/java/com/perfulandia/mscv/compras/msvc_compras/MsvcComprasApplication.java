f19bee74252e7d3919de8c2408a32f6f50a63dbd
Joaquin-Dev369 <dreikdreik1@gmail.com>
2025-06-16 08:59:42 -0400

    Primer Microser(Venta-incompleto)

---
+++ msvc-compras/src/main/java/com/perfulandia/mscv/compras/msvc_compras/MsvcComprasApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.mscv.compras.msvc_compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcComprasApplication.class, args);
+	}
+
+}
---
+++ msvc-compras/src/test/java/com/perfulandia/mscv/compras/msvc_compras/MsvcComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.mscv.compras.msvc_compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
